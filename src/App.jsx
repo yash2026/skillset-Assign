5b9cd624a644827442c237a812393db2a5a7c703
Yashpal <yash38709@gmail.com>
2024-06-03 16:06:17 +0530

    Improvements

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
+import React, { Suspense, lazy } from "react";
 import { Provider } from "react-redux";
-import Login from "./components/Login";
+// import Login from "./components/Login";
 import store from "./app/store";
 import {
   Navigate,
@@ -7,31 +8,42 @@ import {
   BrowserRouter as Router,
   Routes,
 } from "react-router-dom";
-import Home from "./components/Home";
-import UserSkills from "./components/UserSkills";
+// import Home from "./components/Home";
+// import UserSkills from "./components/UserSkills";
 import PrivateRoute from "./components/PrivateRoute"; // Import the PrivateRoute component
 
+const Login = lazy(() => import("./components/Login"));
+const Home = lazy(() => import("./components/Home"));
+const UserSkills = lazy(() => import("./components/UserSkills"));
 function App() {
   const userData = JSON.parse(localStorage.getItem("userData"));
   return (
     <Provider store={store}>
       <Router>
-        <Routes>
-          <Route path="/login" element={<Login />} />
-          <Route
-            path="/"
-            element={<Navigate to={userData ? "/home" : "/login"} />}
-          />
-          <Route
-            path="*"
-            element={<Navigate to={userData ? "/home" : "/login"} />}
-          />
-          <Route path="/home" element={<PrivateRoute element={Home} />} />
-          <Route
-            path="/user-skills"
-            element={<PrivateRoute element={UserSkills} />}
-          />
-        </Routes>
+        <Suspense
+          fallback={
+            <div className="h-screen flex justify-center items-center">
+              Loading...
+            </div>
+          }
+        >
+          <Routes>
+            <Route path="/login" element={<Login />} />
+            <Route
+              path="/"
+              element={<Navigate to={userData ? "/home" : "/login"} />}
+            />
+            <Route
+              path="*"
+              element={<Navigate to={userData ? "/home" : "/login"} />}
+            />
+            <Route path="/home" element={<PrivateRoute element={Home} />} />
+            <Route
+              path="/user-skills"
+              element={<PrivateRoute element={UserSkills} />}
+            />
+          </Routes>
+        </Suspense>
       </Router>
     </Provider>
   );
